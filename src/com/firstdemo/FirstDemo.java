9c37e4ceb5986a41f49e5d8496dd5fe0682a105b
Dayanand <Dayanand@DESKTOP-2PC6KIQ>
2024-12-22 07:59:20 +0530

    Fist Commit

---
+++ src/com/firstdemo/FirstDemo.java
@@ -0,0 +1,37 @@
+package com.firstdemo;
+
+public class FirstDemo {
+
+}
+class Student{
+	int studId;// primitive
+	String studName;// Non primitive system
+	static float studPer;
+	Student s;// Non primitive User defined
+	 int num1;
+	 int num2;
+	  
+	 int result=num1+num2;
+	 
+	 {// instance block
+		 
+	 }
+	 static{// static block
+		 
+	 }
+	 
+	 int firstDemo() {
+		return 0;
+		 
+	 }
+	 
+	 void secondDemo() {
+		 
+	 }
+	
+	 String thirdDemo() {
+		return "";
+		 
+	 }
+}
+
