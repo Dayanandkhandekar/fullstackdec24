50706fdc3c4b1f2bf46d65ab0e5f28877fdef882
susha <susha@SUSHANT>
2024-12-22 19:04:39 +0530

    Sushant Commit 4 22-12-24

--- src/com/sushantdemo/SushantDemo.java
+++ src/com/sushantdemo/SushantDemo.java
@@ -4,3 +4,19 @@ public class SushantDemo {
 String abc;
 int id=2525;
 }
+class employeeInfo{
+	int employeeId = 12520;
+	char ifPresent='P';
+	String eName = "XYZ";
+	String designation ="General Manager";
+	String eAdreess = "109,GVT CHS, Nashik";
+	String dis = "Nashik";
+	int pinCode = 141414;
+	String emailId= "xyz@gmail.com";
+	float working = 70.50f;
+	String employer ="GVV Transports PVT.LTD";
+	boolean present =true;
+	
+}
+
+
