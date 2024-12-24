d41eb45973fb1bed7fe5a396ca68e774ff79f1e5
Sakshi <Sakshi@LAPTOP-701KL8KG>
2024-12-24 16:16:42 +0530

    sakshi commited 24.12.24

--- src/com/sakshiDemo/SakshiDemo.java
+++ src/com/sakshiDemo/SakshiDemo.java
@@ -3,3 +3,37 @@ package com.sakshiDemo;
 public class SakshiDemo {
 
 }
+class Employee{
+	int empId;
+	String empName;
+	String empDept;
+	
+	
+	static void a1() {
+		System.out.println("Inside Static method");
+		
+	}
+	
+	void a2() {
+		System.out.println("inside instatnce method");
+		
+	}
+	
+	
+	{
+		System.out.println("inside instance block");
+	}
+	
+	static {
+		System.out.println("Inside Static block");
+		
+	}
+	
+	public static void main(String[]args) {
+		System.out.println("inside main");
+		Employee c1=new Employee();
+		Employee d1=new Employee();
+		Employee e1=new Employee();
+		
+	}
+}
