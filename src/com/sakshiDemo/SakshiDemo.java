2be516926cb0700a4122a9fce52bc875199c0735
Sakshi <Sakshi@LAPTOP-701KL8KG>
2024-12-25 18:24:43 +0530

    sakshi commited 25.12.24

--- src/com/sakshiDemo/SakshiDemo.java
+++ src/com/sakshiDemo/SakshiDemo.java
@@ -1,31 +1,46 @@
 package com.sakshiDemo;
 
+
+
 public class SakshiDemo {
 
 }
+
+
 class Employee{
 	int empId;
 	String empName;
 	String empDept;
+	static  int noOfEmployee;
+	static String empProject;
+	
 	
 	
 	static void a1() {
-		System.out.println("Inside Static method");
-		
+		Employee e=new Employee();
+		System.out.println("Inside Static method"+e.empId);
+		System.out.println(" static field"+noOfEmployee);
 	}
 	
 	void a2() {
-		System.out.println("inside instatnce method");
+		System.out.println("inside instatnce method"+empId);
+		System.out.println("static fields"+noOfEmployee);
+		
+
 		
 	}
+	
 	
 	
 	{
 		System.out.println("inside instance block");
+		System.out.println("instance field"+empDept);
 	}
 	
 	static {
+		Employee b=new Employee();
 		System.out.println("Inside Static block");
+		System.out.println("instatnce field"+b.empName);
 		
 	}
 	
@@ -34,6 +49,37 @@ class Employee{
 		Employee c1=new Employee();
 		Employee d1=new Employee();
 		Employee e1=new Employee();
+
 		
 	}
+	
+	
+	class Bank{
+		
+		
+		 
+		 void m2() {
+			 Employee e=new Employee();
+			 System.out.println("instance field"+e.empId);
+			 System.out.println("static field"+Employee.noOfEmployee);
+		 
+		 
+		 }
+		 
+		 {
+			 Employee e=new Employee();
+			 System.out.println("instance field"+e.empName);
+			 System.out.println("static field "+Employee.empProject);
+			 
+			 
+			  
+			 
+		 }
+	 }
+			
+			
+			
+	
+	
 }
+
