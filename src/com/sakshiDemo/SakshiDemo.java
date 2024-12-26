0faa84ab7439c2264ed048f1f8894e56a48e7e4f
Sakshi <Sakshi@LAPTOP-701KL8KG>
2024-12-26 17:39:26 +0530

    sakshi comitted 26.12.24

--- src/com/sakshiDemo/SakshiDemo.java
+++ src/com/sakshiDemo/SakshiDemo.java
@@ -49,8 +49,30 @@ class Employee{
 		Employee c1=new Employee();
 		Employee d1=new Employee();
 		Employee e1=new Employee();
+		System.out.println("e==="+d1);
+   	 String s1=new String("ABcxtn");
+   	
+   	 
+   	 String s2=new String("ABcxtn");
+   	 String s3="abc";
+   	 
+   	 String s5="abc";
+   	
+   	 
+   
+   	 
+   	  System.out.println(s1==s2);
+   	  System.out.println(s3==s5);
+
+   	  
+		
 
 		
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empDept=" + empDept + "]";
 	}
 	
 }
