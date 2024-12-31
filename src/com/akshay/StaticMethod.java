93b75bad880590fee14e95d8775aaa5b334d5f56
user <user@DESKTOP-TNF0CQI.lan>
2024-12-31 19:40:41 +0530

    Akshay commited 31/12

---
+++ src/com/akshay/StaticMethod.java
@@ -0,0 +1,16 @@
+package com.akshay;
+
+public class StaticMethod {
+	static int a=10;
+	static void display()
+	{
+		System.out.println("Static Method");
+		
+	}
+		
+ public static void main(String args[]) {
+	 System.out.println(a);
+	 display();
+ }
+}
+
