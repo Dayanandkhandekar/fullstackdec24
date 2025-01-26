3488a6dcedf9a5218dc921413161dfeb387f0efa
user <user@DESKTOP-TNF0CQI.lan>
2025-01-26 21:34:29 +0530

    akshay commit 26/1/2025

---
+++ src/com/akshay/InterfacePracticeDemo.java
@@ -0,0 +1,27 @@
+package com.akshay;
+
+public class InterfacePracticeDemo {
+
+}
+interface Teacher{
+	void m1();
+}
+interface student{
+	void m2();
+}
+class School implements Teacher,student{
+
+	@Override
+	public void m1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void m2() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
